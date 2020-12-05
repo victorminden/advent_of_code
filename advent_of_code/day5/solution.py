b3fb100ce243ba6b6679a84a419c2249804ac7ac
Victor Minden <victor.minden@pathai.com>
2020-12-05 09:54:09 -0500

    Clean up Day 5 based on binary observation.

--- advent_of_code/day5/solution.py
+++ advent_of_code/day5/solution.py
@@ -1,54 +1,33 @@
-from typing import Optional, List, Tuple, Iterable
+from typing import Set
 from pathlib import Path
 
 from advent_of_code.util import timing
 
 
-BoardingPass = Tuple[str, str]
+BoardingPass = str
+Seat = int
 
 
-def _find_seat_1d(search_str: str, low: str, high: str, seat_list: Optional[List[int]] = None) -> int:
-    # In retrospect, this is just parsing a binary number and so could be simplified dramatically.
-    if seat_list is None:
-        seat_list = list(range(2 ** len(search_str)))
-
-    if not search_str:
-        return seat_list[0]
-
-    middle = (len(seat_list) + 1) // 2
-    if search_str[0] == low:
-        s = slice(0, middle)
-    elif search_str[0] == high:
-        s = slice(middle, None)
-    else:
-        raise ValueError(f"Unknown input character: {search_str[0]}")
-
-    return _find_seat_1d(search_str[1:], low, high, seat_list[s])
+def _pass_to_seat(boarding_pass: BoardingPass) -> Seat:
+    # Improvement based on the observation that the final seat number is just the implied binary number in decimal.
+    # Thanks to @tan.nguyen for pointing this out in discussion.
+    return int(boarding_pass.translate(boarding_pass.maketrans('FBLR', '0101')), base=2)
 
 
-def _find_seat_2d(row: str, col: str) -> int:
-    return 8 * _find_seat_1d(row, low='F', high='B') + _find_seat_1d(col, low='L', high='R')
-
-
-def part1(passes: Iterable[BoardingPass]) -> int:
-    return max(map(lambda p: _find_seat_2d(p[0], p[1]), passes))
-
-
-def part2(passes: Iterable[BoardingPass]) -> int:
-    all_seats = set(map(lambda p: _find_seat_2d(p[0], p[1]), passes))
-    return next(i+1 for i in all_seats if i+1 not in all_seats)
+def part2(seats: Set[Seat]) -> Seat:
+    return next(i + 1 for i in seats if i + 1 not in seats)
 
 
 def main() -> None:
     with open(Path(__file__).parent.joinpath("input.txt")) as file:
-        passes = [(line[:7], line[7:10]) for line in file]
+        boarding_passes = [line.strip() for line in file]
 
     with timing("Part 1"):
-        solution = part1(passes)
+        solution = max(map(_pass_to_seat, boarding_passes))
     print(solution)
 
     with timing("Part 2"):
-        solution = part2(passes)
+        solution = part2(set(map(_pass_to_seat, boarding_passes)))
     print(solution)
 
 
