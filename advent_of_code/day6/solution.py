2e3a0385c150eafb30a54f3fa13258c506e8c228
Victor Minden <victorminden@gmail.com>
2020-12-06 11:00:46 -0500

    Clean up Day 6 with nice symmetric structure

--- advent_of_code/day6/solution.py
+++ advent_of_code/day6/solution.py
@@ -1,14 +1,20 @@
+from typing import List, Set
 from pathlib import Path
+
+Person = Set[str]
+Group = List[Person]
 
 
 def main() -> None:
     with open(Path(__file__).parent.joinpath("input.txt")) as file:
-        groups = [line.strip() for line in file.read().split('\n\n')]
+        lines = [line.strip() for line in file.read().split('\n\n')]
 
-    # Part 1
-    print(sum(len(set(group.strip().replace('\n', ''))) for group in groups))
-    # Part 2
-    print(sum([len(set.intersection(*[set(person) for person in group.split('\n')])) for group in groups]))
+    groups: List[Group] = [[set(person) for person in group.split('\n')] for group in lines]
+
+    print("Part 1: ")
+    print(sum(len(set.union(*group)) for group in groups))
+    print("Part 2: ")
+    print(sum(len(set.intersection(*group)) for group in groups))
 
 
 if __name__ == "__main__":
