934f65fc5ca473c8ce358a43f9c0eb4885278858
Victor Minden <victorminden@gmail.com>
2020-12-07 19:44:02 -0500

    Clean up Python day7

--- advent_of_code/day7/solution.py
+++ advent_of_code/day7/solution.py
@@ -8,35 +8,30 @@ def main() -> None:
     with open(Path(__file__).parent.joinpath("input.txt")) as file:
         rules = [line.strip() for line in file]
 
-    bags = defaultdict(list)
-    bags_reverse = defaultdict(list)
+    bags, bags_reverse = defaultdict(list), defaultdict(list)
     for line in rules:
         src_str, dsts_str = line.split("contain", maxsplit=1)
         src = re.match(r"([\w\s]*) bags", src_str)[1]
-        dsts = re.findall(r"(\d+) ([\w\s]*) bag", dsts_str.strip())
-
-        for (n_bags, bag) in dsts:
+        for (n_bags, bag) in re.findall(r"(\d+) ([\w\s]*) bag", dsts_str.strip()):
             # Build the reverse graph.
             bags_reverse[bag].append(src)
             # Build the forward graph.
             bags[src].append((int(n_bags), bag))
-
+    target_bag = "shiny gold"
+    
     # Part 1
-    already_searched = set()
-    can_hold = set()
-    yet_to_process = ["shiny gold"]
+    already_searched, can_hold = set(), set()
+    yet_to_process = [target_bag]
     while yet_to_process:
-        # Get the next bag to visit.
         bag = yet_to_process.pop()
         already_searched.add(bag)
         next_bags = bags_reverse[bag]
-        if next_bags:
-            can_hold.update(next_bags)
-            yet_to_process.extend([b for b in next_bags if b not in already_searched])
+        can_hold.update(next_bags)
+        yet_to_process.extend([b for b in next_bags if b not in already_searched])
     print(len(can_hold))
 
     # Part 2
-    print(hold_how_many("shiny gold", bags) - 1)
+    print(hold_how_many(target_bag, bags) - 1)  # Subtract 1 to not count the target bag itself.
 
 
 def hold_how_many(bag: str, bags: DefaultDict[str, List[Tuple[int, str]]]) -> int:
