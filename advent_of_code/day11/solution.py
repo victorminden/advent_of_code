d9672bf6593507be9079755d25449628187a9517
Victor Minden <victorminden@gmail.com>
2020-12-11 06:50:55 -0500

    Hacky solution to part2, day 11

--- advent_of_code/day11/solution.py
+++ advent_of_code/day11/solution.py
@@ -10,7 +10,16 @@ from scipy.signal import convolve2d
 from advent_of_code.util import timing
 
 _NEIGHBORS_FILTER = np.array([[1, 1, 1], [1, 0, 1], [1, 1, 1]], dtype=np.int64)
-
+_NEIGHBORS_OFFSETS = [
+    [1, 0],
+    [0, 1],
+    [-1, 0],
+    [0, -1],
+    [1, 1],
+    [-1, -1],
+    [1, -1],
+    [-1, 1],
+]
 
 def part1(seats: np.ndarray) -> int:
     while True:
@@ -21,24 +30,39 @@ def part1(seats: np.ndarray) -> int:
             break
         seats[coming] = '#'
         seats[going] = 'L'
-        print(seats)
 
     return np.sum(seats == '#')
 
 
-def part2(adaptors: List[int]) -> int:
-    @lru_cache(maxsize=None)
-    def num_valid(idx: int, last: int) -> int:
-        if adaptors[idx] > last + 3:
-            # Invalid path, cannot get to the end from here.
-            return 0
-        if idx == len(adaptors) - 1:
-            # Valid path, we are at the end.
-            return 1
-        # Could skip this adaptor, or not.
-        return num_valid(idx + 1, last) + num_valid(idx + 1, adaptors[idx])
+def part2(seats: np.ndarray) -> int:
+    (m, n) = seats.shape
+    while True:
+        visible_seats = np.zeros((m,n), dtype=np.int64)
+        for i in range(m):
+            for j in range(n):
+                for oi, oj in _NEIGHBORS_OFFSETS:
+                    d = 1
+                    ii = i + d * oi
+                    jj = j + d * oj
+                    while ii in range(m) and jj in range(n):
+                        if seats[ii, jj] == '#':
+                            visible_seats[i, j] += 1
+                            break
+                        if seats[ii, jj] != '.':
+                            break
+                        d += 1
+                        ii = i + d * oi
+                        jj = j + d * oj
+        #print(visible_seats)
+        coming = np.logical_and(visible_seats == 0, seats == 'L')
+        going = np.logical_and(visible_seats >= 4.9, seats == '#')
+        if not np.any(coming) and not np.any(going):
+            break
+        seats[coming] = '#'
+        seats[going] = 'L'
+        #print(seats)
+    return np.sum(seats == '#')
 
-    return num_valid(idx=1, last=0)
 
 
 def main() -> None:
@@ -46,12 +70,12 @@ def main() -> None:
         seats = np.array([list(line.strip()) for line in file], dtype='str')
 
     with timing("Part 1"):
-        solution = part1(seats)
+        solution = part1(seats.copy())
     print(solution)
 
-    # with timing("Part 2"):
-    #     solution = part2(adaptors)
-    # print(solution)
+    with timing("Part 2"):
+        solution = part2(seats)
+    print(solution)
 
 
 if __name__ == "__main__":
