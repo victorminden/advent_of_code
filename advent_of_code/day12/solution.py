dfb44d07350623f54570fed80e70041d80a62b8b
Victor Minden <victorminden@gmail.com>
2020-12-12 23:42:46 -0500

    Use complex hack for day 12

--- advent_of_code/day12/solution.py
+++ advent_of_code/day12/solution.py
@@ -5,20 +5,8 @@ from advent_of_code.util import timing
 
 Instruction = Tuple[str, int]
 
-
-class Vec:
-    """A limited 2D vector class to avoid external dependencies."""
-    def __init__(self, x=0, y=0):
-        self._vec = (x, y)
-
-    def __getitem__(self, item):
-        return self._vec[item]
-
-    def __add__(self, other):
-        return Vec(self[0] + other[0], self[1] + other[1])
-
-    def __rmul__(self, other):
-        return Vec(other * self[0], other * self[1])
+# Great hack: represent a 2D vector with a single complex number, and then rotation is easy.
+Vec = complex
 
 
 _DIRECTIONS = {
@@ -30,14 +18,13 @@ _DIRECTIONS = {
 
 
 def _rotate_by_multiple_of_90(vector: Vec, right_or_left: str, angle: int) -> Vec:
-    x, y = vector[0], vector[1]
     while angle != 0:
         if right_or_left == 'R':
-            x, y = -y, x
+            vector *= complex(0, 1)
         else:
-            x, y = y, -x
+            vector *= complex(0, -1)
         angle -= 90
-    return Vec(x, y)
+    return vector
 
 
 def _simulate(instructions: List[Instruction], heading: Vec, is_part2: bool = False) -> int:
@@ -52,7 +39,7 @@ def _simulate(instructions: List[Instruction], heading: Vec, is_part2: bool = Fa
             position += c * heading
         else:
             heading = _rotate_by_multiple_of_90(heading, inst, c)
-    return abs(position[0]) + abs(position[1])
+    return int(abs(position.real) + abs(position.imag))
 
 
 def part1(instructions: List[Instruction]) -> int:
