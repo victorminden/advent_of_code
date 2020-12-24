8b1c76a476d05a27fbe73f3157d0dde07523ece1
Victor Minden <victor.minden@pathai.com>
2020-12-24 09:20:04 -0500

    Day 24 Part 2

--- aoc/day24/solution.py
+++ aoc/day24/solution.py
@@ -1,5 +1,6 @@
-from typing import List
+from typing import List, Tuple, Set
 from pathlib import Path
+import collections
 
 import numpy as np
 
@@ -7,7 +8,7 @@ from aoc.util import timing
 
 
 HexPath = List[str]
-
+TileCoord = Tuple[int, int, int]
 
 _DELTA_R = {
     'e': np.array([1, -1 , 0]),
@@ -20,6 +21,10 @@ _DELTA_R = {
 
 
 def part1(paths: List[HexPath]) -> int:
+    return len(_initial_black_tiles(paths))
+
+
+def _initial_black_tiles(paths: List[HexPath]) -> Set[TileCoord]:
     black_tiles = set()
     for path in paths:
         coord = tuple(sum(_DELTA_R[i] for i in path))
@@ -27,11 +32,36 @@ def part1(paths: List[HexPath]) -> int:
             black_tiles.remove(coord)
         else:
             black_tiles.add(coord)
+    return black_tiles
+
+
+def part2(paths: List[HexPath], num_days=100) -> int:
+    black_tiles = _initial_black_tiles(paths)
+
+    for day in range(num_days):
+        neighbors_of_old_black_tiles = collections.defaultdict(int)
+        for tile in black_tiles:
+            for neighbor in _neighbors(tile):
+                neighbors_of_old_black_tiles[neighbor] += 1
+
+        new_black_tiles = set()
+        for tile, count in neighbors_of_old_black_tiles.items():
+            if count == 2 and tile not in black_tiles:
+                new_black_tiles.add(tile)
+
+        new_white_tiles = set()
+        for tile in black_tiles:
+            if tile not in neighbors_of_old_black_tiles or neighbors_of_old_black_tiles[tile] > 2:
+                new_white_tiles.add(tile)
+
+        black_tiles -= new_white_tiles
+        black_tiles |= new_black_tiles
+
     return len(black_tiles)
 
 
-def part2() -> int:
-    return 0
+def _neighbors(tile: TileCoord):
+    return map(tuple, [np.array(tile) + dr for dr in _DELTA_R.values()])
 
 
 def main() -> None:
@@ -45,9 +75,9 @@ def main() -> None:
         solution = part1(paths)
     print(solution)
 
-    # with timing("Part 2"):
-    #     solution = part2(all_edges, all_ingredients, all_allergens)
-    # print(solution)
+    with timing("Part 2"):
+        solution = part2(paths, num_days=100)
+    print(solution)
 
 
 if __name__ == "__main__":
