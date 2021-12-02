c155f2187608561e7cc191a47179a2da53c3ac19
Victor Minden <victor.minden@cerebras.net>
2021-12-02 08:04:52 -0500

    add solution for day02

---
+++ 2021/aoc/day02/solution.py
@@ -0,0 +1,57 @@
+from dataclasses import dataclass
+from pathlib import Path
+from typing import List, Tuple
+
+from aoc.util import timing
+
+
+@dataclass
+class SubmarineState:
+    x: int = 0
+    y: int = 0
+    aim: int = 0
+
+
+def part1(directions: List[Tuple[str, int]]) -> int:
+    state = SubmarineState()
+    for (dir, scale) in directions:
+        match dir:
+            case "forward":
+                state.x += scale
+            case "down":
+                state.y += scale
+            case "up":
+                state.y -= scale
+    return state.x * state.y
+
+
+def part2(directions: List[int]) -> int:
+    state = SubmarineState()
+    for (dir, scale) in directions:
+        match dir:
+            case "forward":
+                state.x += scale
+                state.y += scale * state.aim
+            case "down":
+                state.aim += scale
+            case "up":
+                state.aim -= scale
+    return state.x * state.y
+
+
+def main() -> None:
+    with open(Path(__file__).parent.joinpath("input.txt")) as file:
+        directions = (line.split() for line in file)
+        directions = [(a, int(b)) for (a, b) in directions]
+
+    with timing("Part 1"):
+        solution = part1(directions)
+    print(solution)
+
+    with timing("Part 2"):
+        solution = part2(directions)
+    print(solution)
+
+
+if __name__ == "__main__":
+    main()
