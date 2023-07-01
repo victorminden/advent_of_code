12ee1b62191c5762fb29e29761ebb7064a0fe32d
Victor Minden <victorminden@gmail.com>
2023-07-01 09:43:40 -0700

    AOC 2022 Day01: add ocaml solutions
    Add solutions in OCaml for the first day.
    Add basic infrastructure for running solutions.

---
+++ 2022/ocaml/aoc/bin/main.ml
@@ -0,0 +1,20 @@
+open Core
+
+let ( >> ) f g x = g (f x)
+
+let day_runner = function
+  | 1, 'a' -> Aoc.Day01.part_one >> string_of_int >> print_endline
+  | 1, 'b' -> Aoc.Day01.part_two >> string_of_int >> print_endline
+  | _ -> failwith "unrecognized day or part"
+
+let command =
+  Command.basic
+    ~summary:
+      "Run the solution for [day] [part] on the input [filename] and print the \
+       result"
+    (let%map_open.Command day = anon ("day" %: int)
+     and part = anon ("part" %: char)
+     and filename = anon ("filename" %: Filename_unix.arg_type) in
+     fun () -> filename |> In_channel.read_all |> day_runner (day, part))
+
+let () = Command_unix.run ~version:"1.0" ~build_info:"AOC" command
---
+++ 2022/ocaml/aoc/lib/day01.ml
@@ -0,0 +1,48 @@
+open Core
+
+let sums s =
+  let not_empty s = not (String.equal "" s) in
+  let sum_lines s =
+    s |> String.split ~on:'\n' |> List.filter ~f:not_empty
+    |> List.map ~f:Int.of_string |> List.fold ~init:0 ~f:( + )
+  in
+  s
+  |> Str.split (Str.regexp "\n\n")
+  |> List.filter ~f:not_empty |> List.map ~f:sum_lines
+
+let part_one s = s |> sums |> List.fold ~init:Int.min_value ~f:max
+
+let part_two s =
+  let s1, s2 = List.split_n (sums s) 3 in
+  let h = Pairing_heap.of_list s1 ~cmp:( - ) in
+  let process x =
+    if x > Pairing_heap.top_exn h then (
+      Pairing_heap.remove_top h;
+      Pairing_heap.add h x)
+  in
+
+  List.iter s2 ~f:process;
+  Pairing_heap.fold h ~init:0 ~f:( + )
+
+let example_data =
+  {|
+        1000
+        2000
+        3000
+
+        4000
+
+        5000
+        6000
+
+        7000
+        8000
+        9000
+
+        10000
+|}
+  |> String.split ~on:'\n' |> List.tl_exn |> List.map ~f:String.strip
+  |> String.concat ~sep:"\n"
+
+let%test_unit "part_one" = [%test_eq: int] (example_data |> part_one) 24000
+let%test_unit "part_two" = [%test_eq: int] (example_data |> part_two) 45000
