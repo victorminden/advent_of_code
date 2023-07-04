787caacef1056a12f94f547840035424bda52d3d
Victor Minden <victorminden@gmail.com>
2023-07-04 12:20:13 -0700

    AOC 2022: day02: clean up ocaml

--- 2022/ocaml/aoc/lib/day02.ml
+++ 2022/ocaml/aoc/lib/day02.ml
@@ -1,51 +1,46 @@
 open Core
 open Util
 
-let shape_score = function
-  | 'X' -> 1
-  | 'Y' -> 2
-  | 'Z' -> 3
-  | _ -> failwith "Bad shape"
+let score (elf, you) =
+  let shape_score = function
+    | 'X' -> 1
+    | 'Y' -> 2
+    | 'Z' -> 3
+    | _ -> failwith "Bad shape"
+  in
+  let pair_score = function
+    | 'A', 'X' | 'B', 'Y' | 'C', 'Z' -> 3
+    | 'A', 'Y' | 'B', 'Z' | 'C', 'X' -> 6
+    | 'A', 'Z' | 'B', 'X' | 'C', 'Y' -> 0
+    | _ -> failwith "Bad pair"
+  in
+  pair_score (elf, you) + shape_score you
 
-let pair_score = function
-  | 'A', 'X' | 'B', 'Y' | 'C', 'Z' -> 3
-  | 'A', 'Y' | 'B', 'Z' | 'C', 'X' -> 6
-  | 'A', 'Z' | 'B', 'X' | 'C', 'Y' -> 0
-  | _ -> failwith "Bad pair"
+let list_of_pairs = String.split_lines >> List.map ~f:(fun s -> (s.[0], s.[2]))
 
-let list_of_pairs s =
-  s |> String.split ~on:'\n' |> List.filter ~f:not_empty
-  |> List.map ~f:(fun s -> (s.[0], s.[2]))
+let part_one =
+  list_of_pairs >> List.fold ~init:0 ~f:(fun acc pair -> acc + score pair)
 
-let part_one s =
-  s |> list_of_pairs
-  |> List.fold ~init:0 ~f:(fun acc (elf, you) ->
-         acc + pair_score (elf, you) + shape_score you)
+let part_two =
+  let remap_you = function
+    | 'A', 'X' -> ('A', 'Z')
+    | 'A', 'Y' -> ('A', 'X')
+    | 'A', 'Z' -> ('A', 'Y')
+    | 'B', 'X' -> ('B', 'X')
+    | 'B', 'Y' -> ('B', 'Y')
+    | 'B', 'Z' -> ('B', 'Z')
+    | 'C', 'X' -> ('C', 'Y')
+    | 'C', 'Y' -> ('C', 'Z')
+    | 'C', 'Z' -> ('C', 'X')
+    | _ -> failwith "Bad pair"
+  in
+  list_of_pairs >> List.map ~f:remap_you
+  >> List.fold ~init:0 ~f:(fun acc pair -> acc + score pair)
 
-let remap_you = function
-  | 'A', 'X' -> ('A', 'Z')
-  | 'A', 'Y' -> ('A', 'X')
-  | 'A', 'Z' -> ('A', 'Y')
-  | 'B', 'X' -> ('B', 'X')
-  | 'B', 'Y' -> ('B', 'Y')
-  | 'B', 'Z' -> ('B', 'Z')
-  | 'C', 'X' -> ('C', 'Y')
-  | 'C', 'Y' -> ('C', 'Z')
-  | 'C', 'Z' -> ('C', 'X')
-  | _ -> failwith "Bad pair"
-
-let part_two s =
-  s |> list_of_pairs |> List.map ~f:remap_you
-  |> List.fold ~init:0 ~f:(fun acc (elf, you) ->
-         acc + pair_score (elf, you) + shape_score you)
-
-let example_data =
-  {|
-        A Y
-        B X
-        C Z
-|} |> String.split ~on:'\n'
-  |> List.tl_exn |> List.map ~f:String.strip |> String.concat ~sep:"\n"
+let example_data = {|A Y
+B X
+C Z
+|}
 
 let%test_unit "part_one" = [%test_eq: int] (example_data |> part_one) 15
 let%test_unit "part_two" = [%test_eq: int] (example_data |> part_two) 12
