a895b34e91d289da313415c632f670d7b74738b8
Victor Minden <victorminden@gmail.com>
2023-12-02 11:11:48 -0800

    2023 Day02

---
+++ 2023/kotlin/aoc/day02/Part1.kt
@@ -0,0 +1,48 @@
+import kotlin.math.max
+
+typealias GameId = Int
+
+data class SetSignature(val redCount: Int, val greenCount: Int, val blueCount: Int)
+
+typealias Game = List<SetSignature>
+
+fun parseGameId(s: String): GameId = s.split(" ").getOrNull(1)?.toInt() ?: throw IllegalStateException("ruh roh!")
+
+fun parseGame(s: String): Game =
+    s.split("; ").map { setString ->
+        val colorToCount =
+            setString.split(", ").associate { countColorString ->
+                val (countString, color) = countColorString.split(" ")
+                color to countString.toInt()
+            }
+        SetSignature(
+            redCount = colorToCount["red"] ?: 0,
+            greenCount = colorToCount["green"] ?: 0,
+            blueCount = colorToCount["blue"] ?: 0,
+        )
+    }
+
+fun reduceGame(game: Game): SetSignature =
+    game.reduce { acc, element ->
+        SetSignature(
+            redCount = max(acc.redCount, element.redCount),
+            greenCount = max(acc.greenCount, element.greenCount),
+            blueCount = max(acc.blueCount, element.blueCount),
+        )
+    }
+
+fun main() {
+    val games: Map<GameId, Game> =
+        System.`in`.bufferedReader().lineSequence().associate { line ->
+            val (gameIdString, gameString) = line.trim().split(": ")
+            parseGameId(gameIdString) to parseGame(gameString)
+        }
+
+    games.filter { (_, game) ->
+        with(reduceGame(game)) {
+            redCount <= 12 && greenCount <= 13 && blueCount <= 14
+        }
+    }.keys.sum().let { result: Int ->
+        println(result)
+    }
+}
